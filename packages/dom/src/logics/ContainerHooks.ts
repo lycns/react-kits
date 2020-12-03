5db84137a246ddd3374477dba4f45a88cc23dc1f
lynnkoo <lynnkoo.1120@gmail.com>
2020-12-03 12:52:53 +0800

    update

--- packages/dom/src/logics/ContainerHooks.ts
+++ packages/dom/src/logics/ContainerHooks.ts
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { AppContext } from '../containers/AppContainer'
 import { transformStyles } from '../utils/style'
-import { DeviceTypes, BroswerTypes } from '@basic-kits/dom'
+import { initWindowHeight } from '@basic-kits/dom'
 import { useLocation } from 'react-router-dom'
 
 export function useAppContext() {
@@ -34,27 +34,9 @@ export function useThemeStyles(styles: any, themes?: any) {
   return transformStyles(styles, { themes, currentTheme: theme })
 }
 
-function initInnerHeight(rootNode: any) {
-  // windows 如果不减去两个像素就会出现滚动条
-  const heightOffset = DeviceTypes.isWindows ? -2 : 0
-  const innerHeight = window.innerHeight
-  rootNode.style.minHeight = innerHeight
-    ? innerHeight + heightOffset + 'px'
-    : '100vh'
-}
-
 export function useInitRootHeight() {
   const { pathname } = useLocation()
   React.useEffect(() => {
-    const rootNode = document.getElementById('root') as any
-    if (!rootNode.style.minHeight) {
-      // 初始化 min height， 主要目的为兼容 safari 的 innerHeight
-      initInnerHeight(rootNode)
-    } else if (BroswerTypes.isWechatWebview) {
-      // WORKAROUND 兼容 wechat 内置浏览器路由切换时 innerHeight 不一致的问题, 路由延迟大概 100 ms
-      setTimeout(() => {
-        initInnerHeight(rootNode)
-      }, 100)
-    }
+    initWindowHeight('root')
   }, [pathname])
 }
